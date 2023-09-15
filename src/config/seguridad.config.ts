ce7c7252251368d7e1da0afde0a3c50897446d2f
NaTaLiA7674 <130678658+NaTaLiA7674@users.noreply.github.com>
2023-09-15 14:43:57 -0500

    Creación de estrategias de autenticación propia

--- src/config/seguridad.config.ts
+++ src/config/seguridad.config.ts
@@ -1,3 +1,9 @@
 export namespace ConfiguracionSeguridad {
   export const claveJWT: string = "Admin@2023*";
+  export const menuUsuarioId = "64fbb2f6352cba33ccfdad56";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
 }
